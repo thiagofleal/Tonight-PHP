dbf3973257fa920d3cd6dbfa205254703c3b1c22
thiagofleal <thiagofernandesleal@yahoo.com.br>
2019-12-02 19:47:14 -0300

    Update Template class

--- src/view/Template.class.php
+++ src/view/Template.class.php
@@ -5,13 +5,25 @@ namespace Tonight\View;
 class Template {
 
 	private $filename;
+	private $data;
 
 	public function __construct(string $filename) {
 		$this->setFileName($filename);
+		$this->data = array();
 	}
 
 	public function setFileName(string $filename) { $this->filename = $filename; }
 	public function getFileName() { return $this->filename; }
+	public function setData($data) { $this->data = $data; }
+	public function getData() { return $this->data; }
+
+	public function setVariable(string $varName, $value) {
+		$this->data[$varName] = $value;
+	}
+
+	public function getVariable(string $varName) {
+		return $this->data[$varName];
+	}
 
 	private static function loadFile(string $filename, $data = array(), $content = NULL, $ignore = false) {
 		if(file_exists($filename)) {
@@ -23,19 +35,19 @@ class Template {
 				$page = str_replace("{{".$key."}}", $value, $page);
 			}
 			if($ignore) {
-				$page = str_replace("\\{", "{", $page);
+				$page = str_replace("{\\{", "{{", $page);
 			}
 			return $page;
 		}
 		return "";
 	}
 
-	public function load(string $content = NULL, $data = array()) {
-		return self::loadFile($this->filename, $data, $content, true);
+	public function load(string $content) {
+		return self::loadFile($this->filename, $this->data, $content, true);
 	}
 
-	public function render(string $content = NULL, $data = array()) {
-		$page = $this->load($content, $data);
+	public function render(string $content) {
+		$page = $this->load($content);
 		echo $page;
 	}
 }
