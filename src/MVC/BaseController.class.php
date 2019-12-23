e7eebf538345ee1fe6dc336406fe176f948d5b03
thiagofleal <thiagofernandesleal@yahoo.com.br>
2019-12-23 17:14:20 -0300

    Update Tonight\MVC\BaseController

---
+++ src/MVC/BaseController.class.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tonight\MVC;
+
+use Tonight\View\Template;
+
+class BaseController {
+
+	private $template;
+	private $directory = '';
+	private $extension = 'php';
+
+	public function __construct($template = NULL) {
+		$this->setTemplate($template);
+	}
+
+	public function getTemplate() { return $this->template; }
+	public function setTemplate($template) {
+		if($template instanceof Template) {
+			$this->template = $template;
+		}
+		else {
+			$this->template = new Template;
+		}
+	}
+
+	public function getDirectory() { return $this->directory; }
+	public function setDirectory(string $directory) { $this->directory = $directory; }
+
+	public function getExtension() { return $this->extension; }
+	public function setExtension(string $extendion) { $this->extendion = $extendion; }
+
+	public function setVariable(string $key, $value) {
+		$this->template->setVariable($key, $value);
+	}
+
+	public function renderView(string $page) {
+		$this->template->require($this->getDirectory().$page.'.'.$this->getExtension());
+	}
+}
