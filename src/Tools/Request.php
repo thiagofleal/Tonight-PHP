1ade3ce3cea494ba4cb948aa6fa5b265dba6c043
thiagofleal <thiagofernandesleal@yahoo.com.br>
2021-03-10 21:09:38 -0300

    Fixing Tonight\Tools\Request::getHttpHeaders()

--- src/Tools/Request.php
+++ src/Tools/Request.php
@@ -167,28 +167,7 @@ class Request
 	}
 
 	public static function getHttpHeaders() {
-		$out = array();
-		foreach($_SERVER as $key => $value) {
-			if (substr($key, 0, 5) == "HTTP_") {
-				$key = str_replace(
-					" ",
-					"-",
-					ucwords(
-						strtolower(
-							str_replace(
-								"_",
-								" ",
-								substr($key, 5)
-							)
-						)
-					)
-				);
-				$out[$key] = $value;
-			} else {
-				$out[$key] = $value;
-			}
-		}
-		return $out;
+		return \getallheaders();
 	}
 
 	public static function getHttpHeader($headerName) {
