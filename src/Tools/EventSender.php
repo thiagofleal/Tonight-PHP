3bc198aac31234c1d5dfa123a070d2db47b4d1e9
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2021-11-10 22:52:13 -0300

    feat: Update EventSender

--- src/Tools/EventSender.php
+++ src/Tools/EventSender.php
@@ -7,6 +7,8 @@ use json_encode;
 class EventSender {
     private $formatter;
     private $callback;
+    private $atExit;
+    private $timeout = 30;
 
     const TEXT = 0;
     const JSON = 1;
@@ -26,38 +28,65 @@ class EventSender {
         }
         $this->formatter = $formatter;
         $this->callback = null;
+        $this->atExit = null;
     }
 
     public function register(callable $callback) {
         $this->callback = $callback;
     }
 
-    public function send(string $event, string $id, array $data) {
+    public function onExit(callable $callback) {
+        $this->atExit = $callback;
+    }
+
+    public function getTimeout() {
+        return $this->timeout;
+    }
+
+    public function setTimeout($timeout) {
+        if (is_int($timeout)) {
+            $this->timeout = $timeout;
+        }
+    }
+
+    private static function flush() {
+        @flush();
+        @ob_end_flush();
+    }
+
+    public function send(string $event, string $id, $data) {
         $formatter = $this->formatter;
 
         echo "event: ".$event.PHP_EOL;
         echo "id: ".$id.PHP_EOL;
         echo "data: ".$formatter($data).PHP_EOL;
         echo PHP_EOL;
-        @flush();
-        @ob_end_flush();
+        self::flush();
     }
 
     public function start($interval = NULL) {
         header('Content-Type: text/event-stream');
         header('Cache-Control: no-cache');
+        self::flush();
+
         if (!is_int($interval)) {
             $interval = 100;
         }
         $interval = intval($interval);
         $count = 0;
         $callback = $this->callback;
+        $exit = $this->atExit;
+        $ends = time() + $this->timeout;
 
-        while (!connection_aborted()) {
+        while (!connection_aborted() && time() < $ends) {
             if (is_callable($callback)) {
                 $callback($this, $count++);
             }
+            self::flush();
             usleep($interval * 1000);
+        }
+        if (is_callable($exit)) {
+            $exit();
         }
     }
 }
