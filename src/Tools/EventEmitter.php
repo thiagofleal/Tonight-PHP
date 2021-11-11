108f5a32154dcf6671bffe01051e6e76147c2ff4
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2021-11-10 22:52:35 -0300

    feat: Update EventSender

--- src/Tools/EventEmitter.php
+++ src/Tools/EventEmitter.php
@@ -10,6 +10,8 @@ class EventEmitter
     private $id;
     private $fileName;
     private $dir;
+    private $expires = 10;
+    private $timeout = NULL;
     private static $baseFileName = "event_{id}.sse";
 
     public function __construct($id) {
@@ -18,15 +20,38 @@ class EventEmitter
         $this->dir = sys_get_temp_dir();
     }
 
+    public function setTimeout($timeout) {
+        if (is_numeric($timeout)) {
+            $this->timeout = intval($timeout);
+        } else {
+            $this->timeout = NULL;
+        }
+    }
+
+    public function getTimeout() {
+        return $this->timeout;
+    }
+
+    public function getExpires() {
+        return $this->expires;
+    }
+
+    public function setExpires($expires) {
+        if (is_numeric($expires)) {
+            $this->expires = intval($expires);
+        }
+    }
+
     public function emit($event, $data = array(), $id = NULL) {
         $fileName = "{$this->dir}/{$this->fileName}";
         if ($id === NULL) {
-            $id = time().rand(0, 9999);
+            $id = "".time().rand(0, 9999);
         }
         $content = array(
             "event" => $event,
             "id" => $id,
-            "data" => json_encode($data)
+            "data" => json_encode($data),
+            "expires" => time() + $this->expires
         );
         $file = fopen($fileName, "a");
         fwrite($file, json_encode($content).PHP_EOL);
@@ -36,54 +61,50 @@ class EventEmitter
     public function subscribe($interval = NULL, array $events = NULL) {
         Session::start();
 
-        header('Content-Type: text/event-stream');
-        header('Cache-Control: no-cache');
-        @flush();
-        @ob_end_flush();
-        
-        if (!is_int($interval)) {
-            $interval = 100;
-        }
+        $sender = new EventSender(EventSender::TEXT);
+
         $fileName = "{$this->dir}/{$this->fileName}";
         $interval = intval($interval);
         $file = fopen($fileName, "a");
-        $sent = array();
         
-        if (Session::isset("sent")) {
-            $sent = Session::get("sent");
+        if (is_int($this->timeout)) {
+            $sender->setTimeout($this->timeout);
         }
-        while (!connection_aborted()) {
+        $sender->register( function($self) use($fileName, $events) {
             $content = file_get_contents($fileName);
             $items = explode(PHP_EOL, trim($content));
-            
-            foreach ($items as $item) {
+            $sent = Session::getOrDefault("sent", array());
+
+            foreach ($items as $key => $item) {
                 $json = json_decode($item);
 
                 if (!empty($json->id)) {
                     $event = $json->event;
                     $id = $json->id;
                     $data = $json->data;
+                    $expires = intval($json->expires);
 
+                    if ($expires < time()) {
+                        unset($items[$key]);
+                        continue;
+                    }
                     if ($events !== NULL) {
                         if (!in_array($event, $events)) {
-                            break;
+                            continue;
                         }
                     }
                     if (!in_array($id, $sent)) {
-                        echo "event: ".$event.PHP_EOL;
-                        echo "id: ".$id.PHP_EOL;
-                        echo "data: ".$data.PHP_EOL;
-                        echo PHP_EOL;
-                        @flush();
-                        @ob_end_flush();
+                        $self->send($event, $id, $data);
                         $sent[] = $id;
                         Session::set("sent", $sent);
                     }
                 }
             }
-            usleep($interval * 1000);
-        }
-        fclose($file);
-        unlink($fileName);
+            file_put_contents($fileName, implode(PHP_EOL, $items).PHP_EOL);
+        });
+        $sender->onExit( function() use($file) {
+            fclose($file);
+        });
+        $sender->start($interval);
     }
 }
